fe96c5d0fcf025db712614342b8c2708064efb4b
Berbigier thomas <thomas.berbigier@gmail.com>
2024-08-11 12:55:31 +0200

    Ajout des routes

--- src/app/pages/page-inscription/page-inscription.component.ts
+++ src/app/pages/page-inscription/page-inscription.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faCheckSquare} from '@fortawesome/free-solid-svg-icons';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-page-inscription',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './page-inscription.component.html',
   styleUrl: './page-inscription.component.scss'
 })
--- src/app/pages/page-login/page-login.component.ts
+++ src/app/pages/page-login/page-login.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faCheckSquare} from '@fortawesome/free-solid-svg-icons';
+import {RouterLink} from "@angular/router";
+import {PageInscriptionComponent} from "../page-inscription/page-inscription.component";
 
 @Component({
   selector: 'app-page-login',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './page-login.component.html',
   styleUrl: './page-login.component.scss'
 })
 export class PageLoginComponent {
   protected readonly faCheckSquare = faCheckSquare;
+  protected readonly PageInscriptionComponent = PageInscriptionComponent;
 }
