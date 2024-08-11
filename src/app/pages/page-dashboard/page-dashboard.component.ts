ab46902c76c4a171319db1c47270eabe0ce7ad30
Berbigier thomas <thomas.berbigier@gmail.com>
2024-08-11 13:02:22 +0200

    Ajout page statistiques

--- src/app/pages/page-dashboard/page-dashboard.component.ts
+++ src/app/pages/page-dashboard/page-dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-page-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './page-dashboard.component.html',
   styleUrl: './page-dashboard.component.scss'
 })
