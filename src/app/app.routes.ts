152ec3c5620c187263c5c7e5f5ddbec70c745a83
Berbigier thomas <thomas.berbigier@gmail.com>
2024-08-11 14:38:58 +0200

    Ajout page dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiquesComponent} from "./pages/page-statistiques/page-statistiques.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'Login',
+    component: PageLoginComponent
+  },
+  {
+    path: 'Inscription',
+    component: PageInscriptionComponent
+  },
+  {
+    path: '',
+    component: PageDashboardComponent,
+    children: [
+      {
+        path: 'Statistiques',
+        component: PageStatistiquesComponent
+      }
+    ]
+  }
+];
