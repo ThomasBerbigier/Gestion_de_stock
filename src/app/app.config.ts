fe9358f3d8b4edbcc6825b0a4b765b34ba424c6d
Berbigier thomas <thomas.berbigier@gmail.com>
2024-09-15 17:04:44 +0200

    Implementation intercepteur Http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient} from "@angular/common/http";
+import {HttpInterceptorService} from "./services/interceptor/http-interceptor.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {provide: HTTP_INTERCEPTORS,
+    useClass: HttpInterceptorService,
+    multi: true}
   ]
 };
--- src/app/pages/profil/changer-mot-de-passe/changer-mot-de-passe.component.ts
+++ src/app/pages/profil/changer-mot-de-passe/changer-mot-de-passe.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faSquarePlus, faXmark} from "@fortawesome/free-solid-svg-icons";
 import {Router} from "@angular/router";
+import {CategoriesService} from "../../../../gs-api/src/services/categories.service";
 
 @Component({
   selector: 'app-changer-mot-de-passe',
@@ -12,14 +13,21 @@ import {Router} from "@angular/router";
   templateUrl: './changer-mot-de-passe.component.html',
   styleUrl: './changer-mot-de-passe.component.scss'
 })
-export class ChangerMotDePasseComponent {
+export class ChangerMotDePasseComponent implements OnInit {
 
   protected readonly faXmark = faXmark;
   protected readonly faSquarePlus = faSquarePlus;
 
   constructor(
+    private utili: CategoriesService,
     private router: Router
   ) {
+  }
+
+  ngOnInit(): void {
+    this.utili.findAll7()
+      .subscribe(res => {
+    });
   }
 
   cancel(): void {
